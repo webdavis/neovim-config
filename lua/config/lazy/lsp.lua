d67982fe88172b405367cc3a78f201db88c1640f
Stephen A. Davis <stephen@webdavis.io>
2024-02-16 13:48:37 -0700

    Change ansible language server `filetypes` detection and fix code indentation

--- lua/config/lazy/lsp.lua
+++ lua/config/lazy/lsp.lua
@@ -102,147 +102,150 @@ return {
     end,
   },
   config = function()
-  -- IMPORTANT: make sure to setup neodev BEFORE lspconfig.
-  require("neodev").setup({
-    -- Add any options here, or leave empty to use the default settings.
-  })
+    -- IMPORTANT: make sure to setup neodev BEFORE lspconfig.
+    require("neodev").setup({
+      -- Add any options here, or leave empty to use the default settings.
+    })
 
-  -- Then setup your lsp server as usual.
-  local lspconfig = require('lspconfig')
+    -- Then setup your lsp server as usual.
+    local lspconfig = require('lspconfig')
 
-  -- Example to setup lua_ls and enable call snippets.
-  lspconfig.lua_ls.setup({
-    settings = {
-      Lua = {
-        completion = {
-          callSnippet = "Replace"
+    -- Example to setup lua_ls and enable call snippets.
+    lspconfig.lua_ls.setup({
+      settings = {
+        Lua = {
+          completion = {
+            callSnippet = "Replace"
+          }
         }
       }
-    }
-  })
+    })
 
-  local cmp = require('cmp')
-  local cmp_lsp = require('cmp_nvim_lsp')
-  local capabilities = vim.tbl_deep_extend(
-    'force',
-    {},
-    vim.lsp.protocol.make_client_capabilities(),
-    cmp_lsp.default_capabilities())
+    local cmp = require('cmp')
+    local cmp_lsp = require('cmp_nvim_lsp')
+    local capabilities = vim.tbl_deep_extend(
+      'force',
+      {},
+      vim.lsp.protocol.make_client_capabilities(),
+      cmp_lsp.default_capabilities())
 
-  require('fidget').setup({})
-  require('mason').setup()
-  require('mason-lspconfig').setup({
-    ensure_installed = {
-      'ansiblels',
-      'bashls',
-      'clangd',
-      'docker_compose_language_service',
-      'dockerls',
-      'html',
-      'lua_ls',
-      'pyright',
-      'rust_analyzer',
-      'terraformls',
-      'tflint',
-      'tsserver',
-      'zls',
-    },
-    handlers = {
-      function(server_name) -- default handler (optional)
+    require('fidget').setup({})
+    require('mason').setup()
+    require('mason-lspconfig').setup({
+      ensure_installed = {
+        'ansiblels',
+        'bashls',
+        'clangd',
+        'docker_compose_language_service',
+        'dockerls',
+        'html',
+        'lua_ls',
+        'pyright',
+        'rust_analyzer',
+        'terraformls',
+        'tflint',
+        'tsserver',
+        'zls',
+      },
+      handlers = {
+        function(server_name) -- default handler (optional)
 
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities
-        }
-      end,
+          require('lspconfig')[server_name].setup {
+            capabilities = capabilities
+          }
+        end,
 
-      ['lua_ls'] = function()
-        local lspconfig = require('lspconfig')
-        lspconfig.lua_ls.setup {
-          capabilities = capabilities,
-          settings = {
-            Lua = {
-              diagnostics = {
-                globals = { 'vim', 'it', 'describe', 'before_each', 'after_each' },
+        ['lua_ls'] = function()
+          local lspconfig = require('lspconfig')
+          lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { 'vim', 'it', 'describe', 'before_each', 'after_each' },
+                }
               }
             }
           }
-        }
+        end,
+      }
+    })
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          require('luasnip').lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-i>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.config.disable,
+        ['<C-c>'] = function(fallback) cmp.abort() fallback() end,
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+        { name = 'calc' },
+        { name = 'git' },
+        { name = 'rg' },
+      }, {
+          { name = 'buffer' },
+        })
+    })
+
+    vim.diagnostic.config({
+      -- update_in_insert = true,
+      float = {
+        focusable = false,
+        style = 'minimal',
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+      },
+    })
+
+    require('lspconfig').ansiblels.setup({
+      filetypes = { 'yaml', 'ansible', 'yaml.ansible' }
+    })
+
+    -- Global mappings.
+    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+    vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+    -- Use LspAttach autocommand to only map the following keys
+    -- after the language server attaches to the current buffer
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+      callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+        -- Buffer local mappings.
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, opts)
+        vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, opts)
+        vim.keymap.set('n', '<leader>lt', vim.lsp.buf.type_definition, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', 'K',          vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gD',         vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gd',         vim.lsp.buf.definition, opts)
+        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
       end,
-    }
-  })
-
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body)
-      end,
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-i>'] = cmp.mapping.confirm({ select = true }),
-      ['<C-e>'] = cmp.config.disable,
-      ['<C-c>'] = function(fallback) cmp.abort() fallback() end,
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' }, -- For luasnip users.
-      { name = 'calc' },
-      { name = 'git' },
-      { name = 'rg' },
-    }, {
-        { name = 'buffer' },
-      })
-  })
-
-  vim.diagnostic.config({
-  -- update_in_insert = true,
-  float = {
-  focusable = false,
-  style = 'minimal',
-  border = 'rounded',
-  source = 'always',
-  header = '',
-  prefix = '',
-},
-})
-
-  -- Global mappings.
-  -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-  vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
-
-  -- Use LspAttach autocommand to only map the following keys
-  -- after the language server attaches to the current buffer
-  vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-      -- Enable completion triggered by <c-x><c-o>
-      vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-      -- Buffer local mappings.
-      -- See `:help vim.lsp.*` for documentation on any of the below functions
-      local opts = { buffer = ev.buf }
-      vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, opts)
-      vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, opts)
-      vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references, opts)
-      vim.keymap.set('n', '<leader>ls', vim.lsp.buf.signature_help, opts)
-      vim.keymap.set('n', '<leader>lt', vim.lsp.buf.type_definition, opts)
-      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set('n', 'K',          vim.lsp.buf.hover, opts)
-      vim.keymap.set('n', 'gD',         vim.lsp.buf.declaration, opts)
-      vim.keymap.set('n', 'gd',         vim.lsp.buf.definition, opts)
-
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-      vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-      vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-      vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
-    end,
-  })
+    })
   end
 }
