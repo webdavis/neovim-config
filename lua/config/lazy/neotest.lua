301fc92bada3e0c539fc9d043cc4410e8cb32797
Stephen A. Davis <stephen@webdavis.io>
2024-02-16 13:47:25 -0700

    Update neotest run all tests mapping

--- lua/config/lazy/neotest.lua
+++ lua/config/lazy/neotest.lua
@@ -23,7 +23,7 @@ return {
 
       vim.keymap.set("n", "<leader>tc", function() neotest.run.run() end, { desc = 'Run nearest' })
       vim.keymap.set("n", "<leader>tf", function() neotest.run.run(vim.fn.expand("%")) end, { desc = 'Run file' })
-      vim.keymap.set("n", "<leader>tw", function() neotest.run.run(vim.loop.cwd()) end, { desc = 'Run all test files (workspace)' })
+      vim.keymap.set("n", "<leader>ta", function() neotest.run.run(vim.loop.cwd()) end, { desc = 'Run all test files (workspace)' })
       vim.keymap.set("n", "<leader>tl", function() neotest.run.run_last() end, { desc = 'Run last test' })
       vim.keymap.set("n", "<leader>th", function() neotest.run.stop() end, { desc = 'Halt tests' })
       vim.keymap.set("n", "<leader>ts", function() neotest.summary.toggle() end, { desc = 'Toggle summary' })
