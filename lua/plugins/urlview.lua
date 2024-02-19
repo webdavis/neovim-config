ca18a816ffd58c339ba6f813103bc2869bab44f5
Stephen A. Davis <stephen@webdavis.io>
2024-02-18 17:03:50 -0700

    Fix lua warning: `setup` requires 2 arguments

--- lua/plugins/urlview.lua
+++ lua/plugins/urlview.lua
@@ -1,7 +1,7 @@
 return {
   "axieax/urlview.nvim",
   config = function()
-    require("urlview").setup()
+    require("urlview").setup({})
     vim.keymap.set('n', '<leader>ur', ":<C-u>UrlView<CR>", {noremap = true})
   end
 }
