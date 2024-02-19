2ecb6a428b784ff855099d46d8442e7b9905b6be
Stephen A. Davis <stephen@webdavis.io>
2024-02-18 17:05:03 -0700

    Add support for barbar diagnostics and disable inactive buffers

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -7,6 +7,21 @@ return {
   opts = {
     icons = {
       button = '×',
+      diagnostics = {
+        [vim.diagnostic.severity.ERROR] = {enabled = true},
+        [vim.diagnostic.severity.WARN] = {enabled = true},
+        [vim.diagnostic.severity.INFO] = {enabled = true},
+        [vim.diagnostic.severity.HINT] = {enabled = true},
+      },
+    },
+    hide = {
+      extensions = true,
+      inactive = true,
+    },
+    gitsigns = {
+      added = {enabled = true, icon = '+'},
+      changed = {enabled = true, icon = '~'},
+      deleted = {enabled = true, icon = '-'},
     },
   }
 }
