dde1675815a088d13921c3082425705f480722f7
Stephen A. Davis <stephen@webdavis.io>
2024-02-21 12:00:59 -0700

    Change `nvim-notify` to static to increase render speed

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,6 +1,10 @@
 return {
   'rcarriga/nvim-notify',
   config = function()
+    require("notify").setup({
+      stages = "static",
+      timeout = 3000,
+    })
     vim.notify = require("notify")
-  end
+  end,
 }
