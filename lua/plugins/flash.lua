455bfe1932f313b18790c19b2243bed32a3d4971
Stephen A. Davis <stephen@webdavis.io>
2024-03-07 21:53:29 -0700

    Turn off `flash` default behavior. Require invoking using keymap

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,27 +1,27 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {
-    labels = "fghjklqwetuopzvbnm",
-    modes = {
-      char = {
-        jump_labels = true
-      }
-    },
-    label = {
-      -- allow uppercase labels
-      uppercase = true,
-    -- add any labels with the correct case here, that you want to exclude
-      exclude = "yCDX",
-    },
-  },
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {
+		modes = {
+			search = {
+				enabled = false,
+			},
+			char = {
+				enabled = false,
+				jump_labels = true,
+			},
+		},
+		label = {
+			-- allow uppercase labels
+			uppercase = true,
+		},
+	},
   -- stylua: ignore
   keys = {
     { "<leader>/", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "flash - search" },
-    { "<leader>h", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "flash - treesitter integration" },
+    { "gs", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "flash - treesitter integration" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "flash - remote" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "flash - treesitter search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "flash - toggle flash style search" },
   },
 }
