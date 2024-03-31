7d086ff6d941503a1763dd0877765744d9ebfd3b
Stephen A. Davis <stephen@webdavis.io>
2024-03-31 15:37:22 -0600

    Refactor keymaps to `map` from `custom_api`

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,63 +1,64 @@
+--------------------
+-- Core Key Mappings
+--------------------
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local map = require("config.custom_api").map
 
 -- Faster file manipulation.
-vim.keymap.set({ "n" }, "<C-s>", "<cmd>write<cr><esc>", { noremap = true, silent = false, desc = "quickly save file" })
-vim.keymap.set({ "n" }, "<C-q>", "<cmd>quit<cr>", { noremap = true, silent = true, desc = "quickly quit the file" })
-vim.keymap.set({ "n" }, "<leader><C-s>", "<cmd>wa<cr>", { noremap = true, silent = false, desc = "write - all files" })
-vim.keymap.set({ "n" }, "<leader>e", "<cmd>new<cr>", { noremap = true, silent = true, desc = "new file" })
-vim.keymap.set({ "n" }, "<leader>0a", "<cmd>qa<cr>", { noremap = true, silent = true, desc = "quit all" })
-vim.keymap.set({ "n" }, "<leader>0f", "<cmd>qa!<cr>", { noremap = true, silent = true, desc = "force quit all" })
+map("n", "<C-s>", "write", "quickly save file", true)
+map("n", "<C-q>", "quit", "quickly quit the file")
+map("n", "<leader><C-s>", "wall", "write - all files")
+map("n", "<leader>e", "new", "new file")
+map("n", "<leader>0a", "qa", "quit all")
+map("n", "<leader>0f", "qa!", "force quit all")
 
 -- https://github.com/pocco81/auto-save.nvim
-vim.keymap.set({ "n" }, "<leader>S", "<cmd>ASToggle<cr>", { noremap = true, silent = false, desc = "autosave - activate" })
+map("n", "<leader>S", "ASToggle", "autosave - activate")
 
 -- Open Lazy screen.
-vim.keymap.set({ "n" }, "<leader>la", "<cmd>Lazy<cr>", { noremap = true, silent = true, desc = "lazy - open lazy dashboard" })
-vim.keymap.set({ "n" }, "<leader>lp", "<cmd>Lazy profile<cr>", { noremap = true, silent = true, desc = "lazy - profile plugins" })
-vim.keymap.set({ "n" }, "<leader>ll", "<cmd>Lazy log<cr>", { noremap = true, silent = true, desc = "lazy - open plugin logs" })
+map("n", "<leader>la", "Lazy", "lazy - open lazy dashboard")
+map("n", "<leader>lp", "Lazy profile", "lazy - profile plugins")
+map("n", "<leader>ll", "Lazy log", "lazy - open plugin logs")
 
 -- Better line shifting.
-vim.keymap.set({ "v" }, "<", "<gv", { desc = "shift left" })
-vim.keymap.set({ "v" }, ">", ">gv", { desc = "shift right" })
+map("v", "<", "<gv", "shift left")
+map("v", ">", ">gv", "shift right")
 
 -- See :help toggleterm.txt
-vim.keymap.set(
-  { "n", "t", "i", "v" },
-  "<C-_>",
-  "<cmd>ToggleTerm<cr>",
-  { noremap = true, silent = true, desc = "toggleterm - open/close" }
-)
+map({ "n", "t", "i", "v" }, "<C-_>", "ToggleTerm", "toggleterm - open/close")
 
 -- Copy, Cut, Paste clipboard integration.
-vim.keymap.set({ "x" }, "<C-c>", '"+y', { noremap = true, desc = "copy to clipboard" })
-vim.keymap.set({ "x" }, "<C-f>", '"+p', { noremap = true, desc = "paste from clipboard" })
+map("x", "<C-c>", '"+y', "copy to clipboard")
+map("x", "<C-f>", '"+p', "paste from clipboard")
 
 -- Better up and down keys when there is line wrapping.
 vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true })
 vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true })
 
 -- Resize windows.
-vim.keymap.set({ "n" }, "<C-Up>", "<cmd>resize +2<cr>", { noremap = true, desc = "increase window height" })
-vim.keymap.set({ "n" }, "<C-Down>", "<cmd>resize -2<cr>", { noremap = true, desc = "decrease window height" })
-vim.keymap.set({ "n" }, "<C-Left>", "<cmd>vertical resize -2<cr>", { noremap = true, desc = "decrease window width" })
-vim.keymap.set({ "n" }, "<C-Right>", "<cmd>vertical resize +2<cr>", { noremap = true, desc = "increase window width" })
+map("n", "<C-Up>", "resize +2", "increase window height")
+map("n", "<C-Down>", "resize -2", "decrease window height")
+map("n", "<C-Left>", "vertical resize -2", "decrease window width")
+map("n", "<C-Right>", "vertical resize +2", "increase window width")
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-vim.keymap.set({ "n" }, "n", "'Nn'[v:searchforward]", { noremap = true, expr = true, desc = "next search result" })
-vim.keymap.set({ "x" }, "n", "'Nn'[v:searchforward]", { noremap = true, expr = true, desc = "next search result" })
-vim.keymap.set({ "o" }, "n", "'Nn'[v:searchforward]", { noremap = true, expr = true, desc = "next search result" })
-vim.keymap.set({ "n" }, "N", "'nN'[v:searchforward]", { noremap = true, expr = true, desc = "prev search result" })
-vim.keymap.set({ "x" }, "N", "'nN'[v:searchforward]", { noremap = true, expr = true, desc = "prev search result" })
-vim.keymap.set({ "o" }, "N", "'nN'[v:searchforward]", { noremap = true, expr = true, desc = "prev search result" })
+map("n", "n", "'Nn'[v:searchforward]", "next search result")
+map("x", "n", "'Nn'[v:searchforward]", "next search result")
+map("o", "n", "'Nn'[v:searchforward]", "next search result")
+map("n", "N", "'nN'[v:searchforward]", "prev search result")
+map("x", "N", "'nN'[v:searchforward]", "prev search result")
+map("o", "N", "'nN'[v:searchforward]", "prev search result")
 
 -- Make the current file executable.
-vim.keymap.set("n", "<leader>X", "<cmd>!chmod +x %<CR>", { silent = false, desc = "make current file executable" })
+map("n", "<leader>X", "!chmod +x %", "make current file executable", true)
 
 -- Replace word under cursor text with last yanked text.
-vim.keymap.set("n", "<leader>p", [[viw"_dP]], { desc = "replace <cword> with yanked text" })
+map("n", "<leader>p", [[viw"_dP]], "replace <cword> with yanked text")
 
 -- Yank to end of line without line-ending character.
-vim.keymap.set("n", "Y", '"+yg_', { desc = "Yank to EOL (without line-ending)" })
+map("n", "Y", '"+yg_', "Yank to EOL (without line-ending)")
 
 -- Toggle quickfix list.
 local toggle_qf = function()
@@ -75,6 +76,6 @@ local toggle_qf = function()
   vim.api.nvim_feedkeys([['"]], "im", false)
 end
 
-vim.keymap.set("n", "<M-0>", function()
+map("n", "<M-0>", function()
   toggle_qf()
-end, { desc = "quickfix - toggle" })
+end, "quickfix - toggle")
