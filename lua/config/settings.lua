892e6497f4cb6d8095af825b472153b3274a1d5f
Stephen A. Davis <stephen@webdavis.io>
2024-03-31 13:38:14 -0600

    Refactor settings code. Primarily, select `vim.opt` over `custom_api.set_option` for option settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,133 +1,127 @@
-local set_option = require("config.custom_api").set_option
+-- Disable netrw.
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.signcolumn = "yes"
 
 -- Turn on directory-specific Neovim settings.
 vim.opt.exrc = true
 
 -- Display the colorcolumn at 1 greater than the buffers textwidth. FIXME
 local textwidth = 95
-set_option("textwidth", { o = textwidth, bo = textwidth })
+vim.opt.textwidth = textwidth
 vim.opt.colorcolumn = tostring(textwidth)
 
 -- Shows where the pattern matches, as it has been typed so far.
-vim.o.incsearch = true
+vim.opt.incsearch = true
 
 -- Enable backspacing over everything in insert mode.
-vim.o.backspace = [[indent,eol,start]]
-
--- See :help fo-table
-vim.cmd([[
-  augroup custom_settings_fo
-    autocmd!
-    autocmd BufEnter * set fo+=t
-    autocmd BufEnter * set fo+=n
-    autocmd BufEnter * set fo+=j
-    autocmd BufEnter * set fo-=o
-  augroup END
-]])
-
--- Specify the offset from the window border when scrolling.
-vim.o.scrolloff = 1
-
--- Window split behavior.
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- "inclusive" means that the last character of the selection is included in an operation.
--- Include the last character of Visual/Select mode selections. It is only used in Visual and Select mode.
-vim.o.selection = "inclusive"
-
--- Set the characters used to separate windows and to indicate folds.
-vim.cmd("set fillchars+=vert:│,fold:―")
-
--- Display ↪ at the beginning of a line when wrap is set, and → at the eol when nowrap is set.
-vim.o.showbreak = "↪ "
-vim.o.listchars = [[tab:>\ ,trail:-,extends:→,precedes:<]]
-
--- Start diff mode with vertical splits.
-vim.cmd("set diffopt+=vertical")
-
--- Override ignorecase option if search pat contains uppercase letters.
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- In Insert mode: use the appropriate number of spaces to insert a <Tab>.
-vim.o.expandtab = true
+vim.opt.backspace = "indent,eol,start"
 
 -- Do not wrap lines by default.
-vim.cmd([[
-  augroup custom_settings_nowrap
-    autocmd!
-    autocmd BufEnter * set nowrap
-  augroup END
-]])
+vim.opt.wrap = false
+
+-- Allow specified keys that move the cursor left/right to move to the previous/next line.
+vim.opt.whichwrap = "b,s,<,>,[,]"
+
+-- See :help fo-table
+local custom_settings_fo = vim.api.nvim_create_augroup("custom_settings_fo", { clear = true })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = custom_settings_fo,
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:append("t")
+    vim.opt.formatoptions:append("n")
+    vim.opt.formatoptions:append("j")
+    vim.opt.formatoptions:remove("o")
+  end,
+})
+
+-- Specify the offset from the window border when scrolling.
+vim.opt.scrolloff = 1
+
+-- Window split behavior.
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- "inclusive" means that the last character of the selection is included in an operation.
+-- Include the last character of Visual/Select mode selections. It is only used in Visual and
+-- Select mode.
+vim.opt.selection = "inclusive"
+
+-- Set the characters used to separate windows and to indicate folds.
+vim.opt.fillchars:append("vert:│,fold:—")
+
+-- Display ↪ at the beginning of a line when wrap is set, and → at the eol when nowrap is set.
+vim.opt.showbreak = "↪ "
+vim.opt.listchars = [[tab:>\ ,trail:-,extends:→,precedes:<]]
+
+-- Start diff mode with vertical splits.
+vim.opt.diffopt:append("vertical")
+
+-- Override ignorecase option if search pat contains uppercase letters.
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- In Insert mode: use the appropriate number of spaces to insert a <Tab>.
+vim.opt.expandtab = true
 
 -- Indent wrapped lines the same amount as the beginning of the line.
-vim.o.breakindent = true
+vim.opt.breakindent = true
 
 -- Wrap long lines at a character in 'breakat', rather than the last character that fits on the screen.
-vim.o.linebreak = true
+vim.opt.linebreak = true
 
 -- At the front of a line, Tab/Backspace will insert/delete blanks according to shiftwidth, but
 -- tabstop and softtabstop will be used elsewhere.
-vim.o.smarttab = true
+vim.opt.smarttab = true
 
 -- Round indent to multiple of shiftwidth.
-vim.o.shiftround = true
+vim.opt.shiftround = true
 
 -- Show matching enclosure for 3 seconds: [],(),{},"",'',<>.
-vim.o.showmatch = true
-vim.o.matchtime = 3
+vim.opt.showmatch = true
+vim.opt.matchtime = 3
 
 -- Sets the mapped key timeout to 3 seconds, and the key code timeout to essentially 0.
-vim.o.timeoutlen = 3000
-vim.o.ttimeoutlen = 1
+vim.opt.timeoutlen = 3000
+vim.opt.ttimeoutlen = 1
 
-vim.o.backup = true
+vim.opt.backup = true
 
 -- Set the backup file location, creating the location if it doesn't exist.
-vim.o.backupdir = vim.fn.stdpath("data") .. "/backup"
-vim.fn.mkdir(vim.o.backupdir, "p")
+local backupdir = vim.fn.stdpath("data") .. "/backup"
+vim.opt.backupdir = backupdir
+vim.fn.mkdir(backupdir, "p")
 
 -- Set the swap file location.
 vim.fn.mkdir(vim.fn.stdpath("data") .. "/swap", "p")
 
 -- Set the undo file location.
-vim.o.undodir = vim.fn.stdpath("data") .. "/undo"
-set_option("undofile", { o = true, bo = true })
-vim.fn.mkdir(vim.o.undodir, "p")
-
--- Allow specified keys that move the cursor left/right to move to the previous/next line.
-vim.o.whichwrap = "b,s,<,>,[,]"
+local undodir = vim.fn.stdpath("data") .. "/undo"
+vim.opt.undodir = undodir
+vim.fn.mkdir(undodir, "p")
+vim.opt.undofile = true
 
 -- Set the number of modelines that are checked for set commands.
-vim.o.modelines = 3
+vim.opt.modelines = 3
 
 -- Decrease update time for the following autocommands: CursorHold, CursorHoldI.
-vim.o.updatetime = 200
+vim.opt.updatetime = 200
 
 -- Make the commandline bigger.
-vim.o.cmdheight = 2
+vim.opt.cmdheight = 2
 
 -- shortmess: This option helps to avoid all the hit-enter prompts caused by file messages,
 -- for example with CTRL-G, and to avoid some other messages. It is a list of flags:
 --   * f: use "(3 of 5)" instead of "(file 3 of 5)".
-vim.cmd("set shortmess-=f")
+vim.opt.shortmess:remove("f")
 
 -- Turn on the mouse.
-vim.o.mouse = "a"
-
--- Disable netrw.
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
-
-----------------------
--- Custom API Settings
-----------------------
-set_option("number", { o = true, wo = true })
-set_option("relativenumber", { o = true, wo = true })
-set_option("shiftwidth", { o = 4, bo = 4 })
-set_option("softtabstop", { o = 4, bo = 4 })
-set_option("signcolumn", { o = "yes", wo = "yes" }) -- Always display the signcolumn.
+vim.opt.mouse = "a"
 
 --------------------
 -- Colorscheme stuff
