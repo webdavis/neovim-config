7d7003e6074c7148143e450aab33e6c3046fe4c7
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-11-23 20:35:14 -0700

    overseer: add convenient keymaps for toggling horizontal (`<M-7>`) and vertical (`<M-8>`) task output buffers

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -358,7 +358,7 @@ return {
 
     map({
       mode = "n",
-      lhs = "<leader>o%",
+      lhs = { "<leader>o%", "<M-8>" },
       rhs = function()
         toggle_runner("OverseerQuickAction open vsplit")
       end,
@@ -367,7 +367,7 @@ return {
 
     map({
       mode = "n",
-      lhs = "<leader>o'",
+      lhs = { "<leader>o'", "<M-7>" },
       rhs = function()
         toggle_runner("OverseerQuickAction open hsplit")
       end,
