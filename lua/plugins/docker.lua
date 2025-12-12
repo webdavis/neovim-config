9c33abcaf94ab7243bdf917eb2ac135c3638b309
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-12-12 09:43:41 -0700

    feat(lazydocker): add plugin `mgierada/lazydocker.nvim` (keybinding: `<leader>D`)

---
+++ lua/plugins/docker.lua
@@ -0,0 +1,22 @@
+return {
+  "mgierada/lazydocker.nvim",
+  dependencies = { "akinsho/toggleterm.nvim" },
+  config = function()
+    require("lazydocker").setup({
+      -- Border options: "single" | "double" | "shadow" | "curved".
+      border = "curved",
+      width = 0.8,
+      height = 0.8,
+    })
+  end,
+  event = "BufRead",
+  keys = {
+    {
+      "<leader>D",
+      function()
+        require("lazydocker").open()
+      end,
+      desc = "Lazydocker: open floating window",
+    },
+  },
+}
