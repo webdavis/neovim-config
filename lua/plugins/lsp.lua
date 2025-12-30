6e0f9cda6ad5bfa8320f29dfd1869e640a81ac5d
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-12-30 13:44:07 -0700

    style(mason-tool-installer): remove obvious code comment

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -151,8 +151,6 @@ return {
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
-      -- a list of all tools you want to ensure are installed upon
-      -- start
       ensure_installed = {
 
         -- Pin version example:
